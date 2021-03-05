35d93a5d292f05f19f8fdca226d16b82f22caa6f
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2021-03-05 17:30:12 +0100

    Popup

--- docs/script.js
+++ docs/script.js
@@ -29,7 +29,8 @@ function colorize (property) {
 
 const map = L.map('map').setView([50.91, 11.59], 12.4)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox/streets-v11',
   tileSize: 512,
@@ -60,6 +61,7 @@ for (const layer of layers) {
     .then(r => r.json())
     .then(function (feature) {
       const l = L.geoJSON(feature, { style: colorize(layer.property) })
+      l.bindPopup(l => l.feature.properties[layer.property])
       control.addBaseLayer(l, layer.layer)
       if (first) {
         l.addTo(map)
